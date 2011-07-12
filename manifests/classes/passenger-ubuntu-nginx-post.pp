409025b81f94309f5acb8ce3d698b86718e59176
Andrew Allen <achew22@gmail.com>
2011-07-12 05:44:57 +0000

    Fix it

--- manifests/classes/passenger-ubuntu-nginx-post.pp
+++ manifests/classes/passenger-ubuntu-nginx-post.pp
@@ -1,1 +1,10 @@
-class rvm::passenger::nginx::ubuntu::post{}
+class rvm::passenger::nginx::ubuntu::post{
+  exec {'Install passenger-nginx':
+    command => "${binpath}rvm ${ruby_version} exec passenger-install-nginx-module --auto --prefix=/opt/nginx --nginx-source-dir=/tmp/nginx-${nginx_version} --extra-configure-flags=\"--conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --sbin-path=/usr/sbin/nginx\"",
+    require => [
+      Rvm_gem['passenger'],
+      Exec["Unpack nginx ${nginx_version}"],
+    ]
+  }
+
+}
--- manifests/classes/passenger-ubuntu-nginx-pre.pp
+++ manifests/classes/passenger-ubuntu-nginx-pre.pp
@@ -23,13 +23,4 @@ class rvm::passenger::nginx::ubuntu::pre {
     ]
   }
 
-  exec {'Install passenger-nginx':
-    command => "passenger-install-nginx-module --auto --prefix=/opt/nginx --nginx-source-dir=/tmp/nginx-${nginx_version} --extra-configure-flags=\"--conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --lock-path=/var/lock/nginx.lock --pid-path=/var/run/nginx.pid --sbin-path=/usr/sbin/nginx\"",
-    path => "/tmp",
-    creates => $binpath,
-    require => [
-      Rvm_gem['passenger'],
-      Exec["Unpack nginx ${nginx_version}"],
-    ]
-  }
 }
